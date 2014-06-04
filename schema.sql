1dc08e9666a194be9c2eaacf95124ce9b0c1ad60
sazl <samiabdin@gmail.com>
2014-06-04 13:41:33 +0400

    Added schema

---
+++ schema.sql
@@ -0,0 +1,97 @@
+create table countries (
+    iso	     varchar(2)   primary key,
+    fullname varchar(200) not null,
+    region   varchar(200) default ''
+);
+
+create table profiletypes (
+    profile  varchar(200) primary key
+);
+
+
+create table staff (
+    index          integer,
+    firstname      varchar(256) not null,
+    lastname       varchar(256) not null,
+    dateofbirth    datetime,
+    title          varchar(256),
+    profile        varchar(200),
+    thuraya        varchar(50),
+    mobile         varchar(50),
+    nationality    varchar(100),
+    type           varchar(200),
+    staffcolorcode varchar(100),
+
+    foreign key (profile) references profiletypes(profile),
+    foreign key (type) references stafftypes(stafftype)
+);
+create table stafftypes (
+    stafftype varchar(200) primary key
+);
+
+create table activities (
+    id            bigserial    primary key,
+    activity      varchar(200) not null,
+    type          varchar(200) not null,
+    country       varchar(2),
+    etcservicemap varchar(256) default '',
+
+    foreign key (country) references countries(iso)
+);
+
+create table activitytypes (
+    id        bigserial    primary key,
+    activity  text         not null,
+    colorcode varchar(100) default ''
+);
+
+create table events (
+    id        bigserial    primary key,
+    event     varchar(256) not null,
+    startdate datetime     default current_timestamp,
+    enddate   datetime     default current_timestamp,
+    colorcode varchar(100) default '',
+);
+
+create table languages (
+    language varchar(200) primary key
+);
+
+-- Join with staff table to get staff's languages
+create table languagemapping (
+    staffid     bigserial,
+    language    varchar(200),
+    primary key (staffid, language),
+    foreign key (staffid) references staff(id),
+    foreign key (language) references languages(language)
+);
+
+create table roles (
+    id         bigserial     primary key,
+    activityid varchar(200),
+    startdate  datetime      default current_timestamp,
+    enddate    datetime      default current_timestamp,
+    location   varchar(200)  default '',
+    foreign key (id) references staffroles(id),
+    foreign key (activityid) references activities(id)
+);
+
+create table staffroles (
+    id         bigserial    primary key,
+    role       number,
+    staffindex number,
+    startdate  datetime,
+    enddate    datetime,
+    location   varchar(200),
+    comments   text          default '',
+    confirmed  --
+);
+
+create table mission (
+    id          bigserial    primary key,
+    missionname varchar(256),
+    location    varchar(200),
+    description text,
+    startdate   datetime,
+    enddate     datetime
+);
