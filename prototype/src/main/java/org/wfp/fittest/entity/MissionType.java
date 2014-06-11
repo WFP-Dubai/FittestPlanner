c7e24eb0eb0f2d26e6b8a23fe733c3542c9a136c
Sami Zeinelabdin <samiabdin@gmail.com>
2014-06-11 10:20:26 +0400

    Update mission-missiontype mapping

--- prototype/src/main/java/org/wfp/fittest/entity/MissionType.java
+++ prototype/src/main/java/org/wfp/fittest/entity/MissionType.java
@@ -1,8 +1,11 @@
 package org.wfp.fittest.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -12,4 +15,7 @@ public class MissionType {
 	@Id
 	@Column(name = "missiontype")
 	private String missionType;
+	
+	@OneToMany(mappedBy = "missionType")
+	private List<Mission> missions;
 }
