c5a13de907517a17ca92a1ddf1d09c9bb88ce7d7
Sami Zeinelabdin <samiabdin@gmail.com>
2014-06-11 15:24:58 +0400

    Fix the repository deployment error when using Autowired (finally :])

--- prototype/src/main/java/org/wfp/fittest/repository/CountryRepository.java
+++ prototype/src/main/java/org/wfp/fittest/repository/CountryRepository.java
@@ -1,10 +1,8 @@
 package org.wfp.fittest.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import org.wfp.fittest.entity.Country;
 
-@Repository
 public interface CountryRepository extends CrudRepository<Country, Integer> {
 
 }
