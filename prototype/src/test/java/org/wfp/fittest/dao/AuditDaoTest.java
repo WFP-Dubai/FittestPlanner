c840a10099a1654078b5f60acb282a93e41228b4
Sami Zeinelabdin <samiabdin@gmail.com>
2014-06-17 02:01:53 +0400

    Add templates for auditdao and staffdao tests

---
+++ prototype/src/test/java/org/wfp/fittest/dao/AuditDaoTest.java
@@ -0,0 +1,182 @@
+package org.wfp.fittest.dao;
+
+import org.junit.Test;
+
+public class AuditDaoTest extends AbstractDaoTest {
+	
+	@Test
+	public void testFindAuditById() {
+		
+	}
+	
+	@Test
+	public void testFindAllAuditsById() {
+		
+	}
+	
+	@Test
+	public void testFindAuditsByDate() {
+		
+	}
+	
+	@Test
+	public void testFindAuditsBeforeDate() {
+		
+	}
+	
+	@Test
+	public void testFindAuditsAfterDate() {
+		
+	}
+	
+	@Test
+	public void testFindAuditsByDuration() {
+		
+	}
+	
+	@Test
+	public void testFindAuditsByOperationType() {
+		
+	}
+	
+	@Test
+	public void testFindAuditsByOldValue() {
+		
+	}
+	
+	@Test
+	public void testFindAuditsByNewValue() {
+		
+	}
+	
+	@Test
+	public void testFindAuditsByOldValueAndNewValue() {
+		
+	}
+	
+	@Test
+	public void testFindAuditsByIPAddressPattern() {
+		
+	}
+	
+	@Test
+	public void testFindAuditsByTableName() {
+		
+	}
+	
+	@Test
+	public void testFindAuditsByUserID() {
+		
+	}
+	
+	@Test
+	public void testDeleteAudit() {
+		
+	}
+	
+	@Test
+	public void testDeleteAllAudits() {
+		
+	}
+	
+	@Test
+	public void testDeleteAuditsByDate() {
+		
+	}
+	
+	@Test
+	public void testDeleteAuditsBeforeDate() {
+		
+	}
+	
+	@Test
+	public void testDeleteAuditsAfterDate() {
+		
+	}
+	
+	@Test
+	public void testDeleteAuditsByDuration() {
+		
+	}
+	
+	@Test
+	public void testDeleteAuditsByOperationType() {
+		
+	}
+	
+	@Test
+	public void testDeleteAuditsByOldValue() {
+		
+	}
+	
+	@Test
+	public void testDeleteAuditsByNewValue() {
+		
+	}
+	
+	@Test
+	public void testDeleteAuditsByOldValueAndNewValue() {
+		
+	}
+	
+	@Test
+	public void testDeleteAuditsByIPAddressPattern() {
+		
+	}
+	
+	@Test
+	public void testDeleteAuditsByTableName() {
+		
+	}
+	
+	@Test
+	public void testDeleteAuditsByUserID() {
+		
+	}
+	
+	@Test
+	public void testSaveAudit() {
+		
+	}
+	
+	@Test
+	public void testFindOperationTypeById() {
+		
+	}
+	
+	@Test
+	public void testFindAllOperationTypes() {
+		
+	}
+	
+	@Test
+	public void testFindOperationTypeByOperationName() {
+		
+	}
+	
+	@Test
+	public void testDeleteOperationType() {
+		
+	}
+	
+	@Test
+	public void testDeleteOperationTypeById() {
+		
+	}
+	
+	@Test
+	public void testDeleteAllOperationTypes() {
+		
+	}
+	
+	@Test
+	public void testDeleteOperationTypeByOperationName() {
+		
+	}
+	
+	@Test
+	public void testSaveOperationType() {
+		
+	}
+	
+}
