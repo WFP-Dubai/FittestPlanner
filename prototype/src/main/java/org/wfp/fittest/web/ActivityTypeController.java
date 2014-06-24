ea3d3c747f1ea2ec352d871b845054371cba14ae
Sami Zeinelabdin <samiabdin@gmail.com>
2014-06-24 14:12:59 +0400

    Add activityType controller and views

---
+++ prototype/src/main/java/org/wfp/fittest/web/ActivityTypeController.java
@@ -0,0 +1,41 @@
+package org.wfp.fittest.web;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.wfp.fittest.entity.ActivityType;
+import org.wfp.fittest.service.ActivityService;
+
+@Controller
+public class ActivityTypeController {
+	private static final Logger logger = LoggerFactory
+			.getLogger(ActivityController.class);
+
+	@Autowired
+	private ActivityService activityService;
+
+	@RequestMapping(value = "/activitytype", method = RequestMethod.GET)
+	public String activityType(Locale locale, Model model) {
+		List<ActivityType> activityTypes = activityService
+				.findAllActivityTypes();
+		model.addAttribute("activityTypes", activityTypes);
+		return "activityType";
+	}
+
+	@RequestMapping(value = "/activitytype/{activityTypeId}", method = RequestMethod.GET)
+	public String activityType(@PathVariable("activityTypeId") Integer activityTypeId, Locale locale,
+			Model model) {
+		ActivityType activityType = activityService
+				.findActivityTypeById(activityTypeId);
+		model.addAttribute("activityType", activityType);
+		return "activityTypeId";
+	}
+}
---
+++ prototype/src/main/webapp/WEB-INF/views/activityType.jsp
@@ -0,0 +1,45 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<jsp:include page="/WEB-INF/templates/meta.jsp"></jsp:include>
+<title>FITTEST Planner</title>
+</head>
+<body>
+	<jsp:include page="/WEB-INF/templates/navbar.jsp"></jsp:include>
+	<div id="wrapper">
+		<jsp:include page="/WEB-INF/templates/sidebar-button.jsp"></jsp:include>
+		<jsp:include page="/WEB-INF/templates/sidebar.jsp"></jsp:include>
+
+		<div id="page-content-wrapper">
+			<div class="content-header"></div>
+
+			<div class="page-content inset">
+				<div class="row">
+					<div class="col-lg-12">
+						<table class="table table-bordered">
+							<thead>
+								<tr>
+									<th>Activity Type</th>
+									<th>Color Code</th>
+								</tr>
+							</thead>
+							<tbody>
+								<c:forEach var="activityType" items="${activityTypes}">
+									<tr>
+										<td>${activityType.activityType}</td>
+										<td>${activityType.colorCode}</td>
+									</tr>
+								</c:forEach>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<jsp:include page="/WEB-INF/templates/jsload.jsp"></jsp:include>
+</body>
+</html>
